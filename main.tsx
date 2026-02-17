fe9c0f88489a9740788b03bb7252f556cc6f3aa5
healthboostcentral2-web <healthboostcentral2@gmail.com>
2026-02-17 14:52:28 +0530

    Refactor component hierarchy with BrowserRouter

--- main.tsx
+++ main.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ErrorBoundary } from "./components/ErrorBoundary";
 import { CartProvider } from "./context/CartContext";
 import { UserProvider } from "./context/UserContext";
 import { WishlistProvider } from "./context/WishlistContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <CartProvider>
-        <UserProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <CartProvider>
           <WishlistProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
+            <App />
           </WishlistProvider>
-        </UserProvider>
-      </CartProvider>
-    </ErrorBoundary>
+        </CartProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
